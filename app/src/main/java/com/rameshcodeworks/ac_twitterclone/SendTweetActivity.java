1d68f69273ff4da5b8ccde8c9c2b4acfee63c567
Ramesh Menon <rameshimac@gmail.com>
2019-01-01 13:16:44 +0530

    Adding HashMap

--- app/src/main/java/com/rameshcodeworks/ac_twitterclone/SendTweetActivity.java
+++ app/src/main/java/com/rameshcodeworks/ac_twitterclone/SendTweetActivity.java
@@ -1,13 +1,78 @@
 package com.rameshcodeworks.ac_twitterclone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class SendTweetActivity extends AppCompatActivity {
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class SendTweetActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtTweet;
+
+    private ListView viewTweetsListView;
+    private Button btnViewTweets;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_tweet);
+
+        edtTweet = findViewById(R.id.edtSendTweet);
+
+        viewTweetsListView = findViewById(R.id.viewTweetsListView);
+        btnViewTweets = findViewById(R.id.btnSendTweet);
+        btnViewTweets.setOnClickListener(this);
+
+//        HashMap<String, Integer> numbers = new HashMap<>();
+//        numbers.put("Number1", 1);
+//        numbers.put("Number2", 2);
+//
+//        FancyToast.makeText(this, numbers.get("Number1") + "", Toast.LENGTH_LONG, FancyToast.WARNING, true).show();
+
+    }
+
+    public void sendTweet(View view) {
+
+        ParseObject parseObject = new ParseObject("MyTweet");
+        parseObject.put("tweet", edtTweet.getText().toString());
+        parseObject.put("user", ParseUser.getCurrentUser().getUsername());
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+        parseObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+
+                if (e == null) {
+
+                    FancyToast.makeText(SendTweetActivity.this, ParseUser.getCurrentUser().getUsername() + " 's Tweet" + "(" + edtTweet.getText().toString() + ")" + " is Saved!!!",
+                            Toast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+                } else {
+
+                    FancyToast.makeText(SendTweetActivity.this, e.getMessage(), Toast.LENGTH_SHORT, FancyToast.ERROR,
+                            true).show();
+                }
+
+                progressDialog.dismiss();
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
