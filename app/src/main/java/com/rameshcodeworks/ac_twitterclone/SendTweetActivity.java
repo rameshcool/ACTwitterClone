3ccd01b6d3930f61a3a8484c964676c07a8691b6
Ramesh Menon <rameshimac@gmail.com>
2019-01-02 21:27:47 +0530

    Adding ListView for Viewing Tweets

--- app/src/main/java/com/rameshcodeworks/ac_twitterclone/SendTweetActivity.java
+++ app/src/main/java/com/rameshcodeworks/ac_twitterclone/SendTweetActivity.java
@@ -7,20 +7,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SendTweetActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText edtTweet;
 
     private ListView viewTweetsListView;
-    private Button btnViewTweets;
+    private Button btnSendTweet, btnViewTweets;
 
 
     @Override
@@ -31,7 +38,8 @@ public class SendTweetActivity extends AppCompatActivity implements View.OnClick
         edtTweet = findViewById(R.id.edtSendTweet);
 
         viewTweetsListView = findViewById(R.id.viewTweetsListView);
-        btnViewTweets = findViewById(R.id.btnSendTweet);
+        btnSendTweet = findViewById(R.id.btnSendTweet);
+        btnViewTweets = findViewById(R.id.btnViewTweets);
         btnViewTweets.setOnClickListener(this);
 
 //        HashMap<String, Integer> numbers = new HashMap<>();
@@ -67,12 +75,43 @@ public class SendTweetActivity extends AppCompatActivity implements View.OnClick
 
                 progressDialog.dismiss();
             }
+
         });
     }
 
 
     @Override
     public void onClick(View v) {
+
+        final ArrayList<HashMap<String, String>> tweetList = new ArrayList<>();
+        final SimpleAdapter adapter = new SimpleAdapter(SendTweetActivity.this, tweetList, android.R.layout.simple_list_item_2, new String[]{"tweetUserName", "tweetValue"}, new int[]{android.R.id.text1, android.R.id.text2});
+        try {
+
+            ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("MyTweet");
+            parseQuery.whereContainedIn("user", ParseUser.getCurrentUser().getList("fanOf"));
+            parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+
+                    if (objects.size() > 0 && e == null) {
+
+                        for (ParseObject tweetObject : objects) {
+
+                            HashMap<String, String> userTweet = new HashMap<>();
+                            userTweet.put("tweetUserName", tweetObject.getString("user"));
+                            userTweet.put("tweetValue", tweetObject.getString("tweet"));
+                            tweetList.add(userTweet);
+                        }
+
+                        viewTweetsListView.setAdapter(adapter);
+                    }
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
 
     }
 }
